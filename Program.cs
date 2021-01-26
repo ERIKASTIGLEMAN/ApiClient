52bed25978a21c824adfa22f5b4d59150d2b5479
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-26 18:25:40 -0500

    Working

--- Program.cs
+++ Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ApiClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Joke
     {
-      Console.WriteLine("Welcome to C#");
+        public int id { get; set; }
+        public string text { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
     }
-  }
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var responseAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/random_ten");
+
+            Console.WriteLine(responseAsString);
+
+
+        }
+    }
 }
